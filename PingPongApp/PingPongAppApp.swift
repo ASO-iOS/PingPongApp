ee89d4a5d0aa6dfe126a415bff9c628d62dd00e9
Yuri Yunitski <mvoznicki@gmail.com>
2023-09-25 11:30:00 +0300

    Initial Commit

---
+++ PingPongApp/PingPongAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongAppApp.swift
+//  PingPongApp
+//
+//  Created by admin on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PingPongAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
